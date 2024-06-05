84193c5f123cc162b81ac00d8b790311bcd9b0d3
Andrew Walwema <awalwema@gmail.com>
2024-06-05 13:38:35 -0700

    remove auth and dashboard

--- config.ts
+++ config.ts
@@ -89,12 +89,12 @@ const config = {
     // OR you can just do this to use a custom color: main: "#f37055". HEX only.
     main: themes["light"]["primary"],
   },
-  auth: {
-    // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
-    loginUrl: "/api/auth/signin",
-    // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
-    callbackUrl: "/dashboard",
-  },
+  // auth: {
+  //   // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
+  //   loginUrl: "/api/auth/signin",
+  //   // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
+  //   callbackUrl: "/dashboard",
+  // },
 } as ConfigProps;
 
 export default config;
