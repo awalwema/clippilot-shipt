15d2401e253072ab9d43af588100fd548de22552
Andrew Walwema <awalwema@gmail.com>
2024-06-05 23:21:06 -0700

    g analytics

--- _app.tsx
+++ _app.tsx
@@ -1,13 +1,14 @@
+import { AppProps } from "next/app";
 import { useEffect } from "react";
 import Script from "next/script";
 import { useRouter } from "next/router";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
+    const handleRouteChange = (url: string) => {
       window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
         page_path: url,
       });
