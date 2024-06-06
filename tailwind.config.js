a460940fdebccd308ebf0394149a0b41571e7aba
Andrew Walwema <awalwema@gmail.com>
2024-06-06 00:16:47 -0700

    fix sitemap please

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,10 +62,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    // Light & dark themes are added by default (it switches automatically based on OS settings)
-    // You can add another theme among the list of 30+
-    // Add "data-theme='theme_name" to any HTML tag to enable the 'theme_name' theme.
-    // https://daisyui.com/
     themes: ["light", "dark"],
   },
 };
